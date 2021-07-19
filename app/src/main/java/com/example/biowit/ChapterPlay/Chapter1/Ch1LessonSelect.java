328982944069bd01e008bf9633b5d7686d082dda
dilahK38 <khalidastig@gmail.com>
2021-07-19 16:52:33 +0800

    Chapter Select

---
+++ app/src/main/java/com/example/biowit/ChapterPlay/Chapter1/Ch1LessonSelect.java
@@ -0,0 +1,14 @@
+package com.example.biowit.ChapterPlay.Chapter1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.biowit.R;
+
+public class Ch1LessonSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch1lessonselect);
+    }
+}
--- app/src/main/java/com/example/biowit/ChapterPlay/ChapterSelect.java
+++ app/src/main/java/com/example/biowit/ChapterPlay/ChapterSelect.java
@@ -1,4 +1,31 @@
 package com.example.biowit.ChapterPlay;
 
-public class ChapterSelect {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.biowit.ChapterPlay.Chapter1.Ch1LessonSelect;
+import com.example.biowit.R;
+
+public class ChapterSelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapterselect);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide(); // hides the action bar.
+
+        Button btn_Ch1LessonSelect = findViewById(R.id.btn_cell);
+        btn_Ch1LessonSelect.setOnClickListener(v -> {
+            startActivity(new Intent(getApplicationContext(), Ch1LessonSelect.class));
+            finish();
+        });
+    }
 }
